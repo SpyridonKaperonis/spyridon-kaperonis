d5e73c4f0e3c261efb4fe85cc876e693cd4765b4
Spyridon Kaperonis <42813038+SpyridonKaperonis@users.noreply.github.com>
2020-03-06 00:25:08 -0600

    JSON&More

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function fn(){
+    var number = document.getElementById("text").value;
+
+    mystorage = window.localStorage;
+    document.getElementById("output").innerHTML = number;
+    
+
+    
+}
